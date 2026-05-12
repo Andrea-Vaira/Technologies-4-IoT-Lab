71b42412e4302794315399d33a8e7569212c93bc
Riccardo Savietto <s326456@studenti.polito.it>
2026-05-12 12:30:41 +0200

    completato esercizio 2 Lab 3 Hardware

--- Lab3/es2_lab3/es2_lab3.ino
+++ Lab3/es2_lab3/es2_lab3.ino
@@ -1,13 +1,20 @@
-//Exercise 2 LAboratory 3
+//Exercise 2 Laboratory 3
 #include <SPI.h> 
 #include <WiFiNINA.h>
 #include <ArduinoHttpClient.h>
-#define SECRET_SSID "Galaxy A55 RS"
-#define SECRET_PASS "RichiS04"
+#define SECRET_SSID "miao"
+#define SECRET_PASS "12345678"
 #define GROUP_NAME "ArduinoGroup6"
 
-char server_address[]="";
-int server_port=8080;
+int LED_PIN=3;
+int TEMP_PIN=A0;
+const int B=4275;
+const long int R0=100000;
+const int T0= 298.13;
+volatile float T=0.0;
+char server_address[]="10.24.110.101";
+int server_port=9090;
+int status= WL_IDLE_STATUS;
 WiFiClient wifi;
 HttpClient client= HttpClient(wifi, server_address, server_port);
 
@@ -16,22 +23,53 @@ void setup() {
   while(!Serial);
   Serial.println("Exercise Lab 3");
 
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(TEMP_PIN, INPUT);
+
   while(status != WL_CONNECTED)
   {
     Serial.print("Attempting to connect ti SSID: ");
     Serial.println(SECRET_SSID);
-    status= WiFi.begin(SECRET_SSID, SECCRET_PASS);
+    status= WiFi.begin(SECRET_SSID, SECRET_PASS);
     delay(5000);
   }
   Serial.print("Connected with IP address: ");
   Serial.println(WiFi.localIP());
-  server.begin();
 }
 
 void loop() {
+
+  int V= analogRead(TEMP_PIN);
+  float R= (1023.0/(float)V -1.0)*R0; 
+  T= (1.0/(log(R/R0)/B + (1.0/T0)))- 273.1;
+
+  Serial.print("Temp: ");
+  Serial.println(T);
+  String body=senMlEncode("temperature", T, "Cel");
+
   client.beginRequest();
   client.post("/log");
-  
+  client.sendHeader("Content-Type", "application/json");
+  client.sendHeader("Content-Length", body.length());
+  client.beginBody();
+  Serial.println(body);
+  client.print(body);
   client.endRequest();
+  int ret= client.responseStatusCode();
+  String resp= client.responseBody();
 
+  delay(5000);
+}
+
+String senMlEncode(String name, float value, String unit)
+{
+  String json = "{";
+  json += "\"bn\":\"" + String(GROUP_NAME) + "\",";
+  json += "\"e\":[{";
+  json += "\"n\":\"" + name + "\",";
+  json += "\"t\":" + String(millis()) + ",";
+  json += "\"v\":" + String(value) + ","; // Questa virgola separa 'v' da 'u' (corretto)
+  json += "\"u\":\"" + unit + "\"";        // Nota i doppi apici \" attorno a unit
+  json += "}]}";
+  return json;
 }
